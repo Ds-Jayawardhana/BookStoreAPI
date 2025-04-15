9a27c12ebd54e30013eb626088b5372dde6b7ef2
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2025-04-16 01:03:16 +0530

    Created the Post method

--- src/main/java/com/university/bookstore/bookstoreapi/resources/AuthorResource.java
+++ src/main/java/com/university/bookstore/bookstoreapi/resources/AuthorResource.java
@@ -4,6 +4,17 @@
  */
 package com.university.bookstore.bookstoreapi.resources;
 
+import com.university.bookstore.bookstoreapi.exception.InvalidInputException;
+import com.university.bookstore.bookstoreapi.model.Author;
+import com.university.bookstore.bookstoreapi.store.Storage;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 /**
  *
  * @author user
@@ -17,6 +28,30 @@ o DELETE /authors/{id}
 o GET /authors/{id}/books*/
 
 
+
+@Path("/authors")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class AuthorResource {
+    final Storage store =new Storage();
+    
+    
+    @POST
+    public Response createAuthors(Author author){
+        if(author.getBiography()==null||author.getName()==null){
+            throw new InvalidInputException("Any of fields Not must be empty");
+        
+        }
+        String authorId=store.createAuthorId();
+        store.getAuthorList().put(authorId, author);
+        return Response.status(Response.Status.CREATED)
+                .build();
+    }
+    
+    @GET
+    public Author getAllAuthors(){
+        
+    }
+    
     
 }
