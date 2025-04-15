f8521715e6866e12ac153c8dfef245f967653034
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2025-04-15 10:06:20 +0530

    Added student data model

--- src/main/java/com/university/bookstore/bookstoreapi/model/Book.java
+++ src/main/java/com/university/bookstore/bookstoreapi/model/Book.java
@@ -9,5 +9,67 @@ package com.university.bookstore.bookstoreapi.model;
  * @author user
  */
 public class Book {
-    
+
+    public Book(String title, String author, String ISBN, int year, int price, int stockQuantity) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.year = year;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+    private String title;
+    private String author;
+    private String ISBN;
+    private int year;
+    private int price;
+    private int stockQuantity;
 }
