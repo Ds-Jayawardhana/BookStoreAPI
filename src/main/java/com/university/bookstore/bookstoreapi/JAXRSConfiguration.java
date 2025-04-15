f1f7a7db000c0d8d45ef4006b88cf6c42101aa83
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2025-04-15 12:47:47 +0530

    Update the application Configuration file to create a Storage Instance of Storageclass

--- src/main/java/com/university/bookstore/bookstoreapi/JAXRSConfiguration.java
+++ src/main/java/com/university/bookstore/bookstoreapi/JAXRSConfiguration.java
@@ -1,5 +1,6 @@
 package com.university.bookstore.bookstoreapi;
 
+import com.university.bookstore.bookstoreapi.store.Storage;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -9,5 +10,10 @@ import javax.ws.rs.core.Application;
  */
 @ApplicationPath("api")
 public class JAXRSConfiguration extends Application {
+    private final Storage store=new Storage();
+    
+    public Storage getStorage(){
+        return store;
+    }
     
 }
