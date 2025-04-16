c41b8731b271f7a7d01863ce13effba0cfbfba98
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2025-04-16 18:40:15 +0530

    updated the application class

---
+++ src/main/java/com/university/bookstore/bookstoreapi/store/ApplicationConfig.java
@@ -0,0 +1,36 @@
+package com.university.bookstore.bookstoreapi.store;
+
+
+
+
+import com.university.bookstore.bookstoreapi.resources.AuthorResource;
+import com.university.bookstore.bookstoreapi.resources.BookResource;
+import com.university.bookstore.bookstoreapi.resources.CartResource;
+import com.university.bookstore.bookstoreapi.resources.CustomerResource;
+import com.university.bookstore.bookstoreapi.resources.OrderResource;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {
+    private final Storage store = new Storage();
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(BookResource.class);
+        classes.add(AuthorResource.class);
+        classes.add(CustomerResource.class);
+        classes.add(CartResource.class);
+        classes.add(OrderResource.class);
+        return classes;
+    }
+
+    public  Storage getStorage() {
+        return store;
+    }
+}
