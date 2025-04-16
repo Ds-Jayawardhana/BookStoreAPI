7e65fe4446db988345d8207a9e6b4afee8806a06
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2025-04-16 17:13:33 +0530

    Created Customer Model Class

--- src/main/java/com/university/bookstore/bookstoreapi/model/Customer.java
+++ src/main/java/com/university/bookstore/bookstoreapi/model/Customer.java
@@ -7,7 +7,58 @@ package com.university.bookstore.bookstoreapi.model;
 /**
  *
  * @author user
+ * 
+ * name, email, and a simple
+ * 
+ * CustomerResource (/customers)
+
  */
 public class Customer {
+
+    public Customer(String customerId, String name, String email, String password) {
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Customer() {
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    private String customerId;
+    private String name;
+    private String email;
+    private String password;
     
 }
