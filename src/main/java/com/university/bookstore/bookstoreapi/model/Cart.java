437675804d87ed34b49ea8ce50adc44b9ca5d4f1
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2025-04-16 19:54:10 +0530

    Created Cart Data Model

--- src/main/java/com/university/bookstore/bookstoreapi/model/Cart.java
+++ src/main/java/com/university/bookstore/bookstoreapi/model/Cart.java
@@ -4,10 +4,39 @@
  */
 package com.university.bookstore.bookstoreapi.model;
 
+import java.util.Map;
+
 /**
  *
  * @author user
  */
 public class Cart {
+
+    public Cart() {
+    }
+
+    public Cart(String customerId, Map<String, Integer> quantity) {
+        this.customerId = customerId;
+        this.quantity = quantity;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public Map<String, Integer> getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Map<String, Integer> quantity) {
+        this.quantity = quantity;
+    }
+    private String customerId;
+    private Map<String,Integer>quantity;
+    
   
 }
