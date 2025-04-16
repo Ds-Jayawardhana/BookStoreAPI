4c1fc8ac5a061cd5da6a4fbfd4361cb55d7029a3
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2025-04-16 21:28:02 +0530

    Added post Method to cart

--- src/main/java/com/university/bookstore/bookstoreapi/resources/CartResource.java
+++ src/main/java/com/university/bookstore/bookstoreapi/resources/CartResource.java
@@ -4,10 +4,57 @@
  */
 package com.university.bookstore.bookstoreapi.resources;
 
+import com.university.bookstore.bookstoreapi.exception.CustomerNotFoundException;
+import com.university.bookstore.bookstoreapi.model.Cart;
+import com.university.bookstore.bookstoreapi.model.Customer;
+import com.university.bookstore.bookstoreapi.store.Storage;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 /**
  *
  * @author user
+o CartResource (/customers/{customerId}/cart)
+o POST /customers/{customerId}/cart/items
+o GET /customers/{customerId}/cart
+o PUT /customers/{customerId}/cart/items/{bookId}
+o DELETE /customers/{customerId}/cart/items/{bookId}
  */
+
+
+@Path("/customer")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CartResource {
+    Storage store=new Storage();
     
+    
+    @POST
+    @Path("/{id}/cart")
+    public Response getAllItems(@PathParam("id") String customerId,Cart incomingCart){
+        Customer searchCart=store.getCustomerList().get(customerId);
+        
+        if(searchCart==null){
+            throw new CustomerNotFoundException("Cart associated with customer cannot find"+customerId);
+        }
+        
+        Cart exsistingCart=store.getCartList().get(customerId);
+        
+        if(exsistingCart==null){
+                Cart cart=new Cart(customerId);
+                cart.setQuantity(incomingCart.getQuantity());
+                store.getCartList().put(customerId, cart);
+        
+        }
+        store.getCartList().put(customerId, exsistingCart);
+        return Response.status(Response.Status.CREATED)
+                .build();
+        
+        
+    }
 }
