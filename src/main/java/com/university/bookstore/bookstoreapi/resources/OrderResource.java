83dd697bd3277a2bd40e98bb9c1c51c8f87d116d
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2025-04-17 20:53:37 +0530

    Added Post Method for the orderResource class

--- src/main/java/com/university/bookstore/bookstoreapi/resources/OrderResource.java
+++ src/main/java/com/university/bookstore/bookstoreapi/resources/OrderResource.java
@@ -4,10 +4,17 @@
  */
 package com.university.bookstore.bookstoreapi.resources;
 
+import com.university.bookstore.bookstoreapi.exception.CustomerNotFoundException;
+import com.university.bookstore.bookstoreapi.exception.InvalidInputException;
+import com.university.bookstore.bookstoreapi.model.Order;
+import com.university.bookstore.bookstoreapi.store.Storage;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -23,6 +30,28 @@ o GET /customers/{customerId}/orders/{orderId}
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class OrderResource {
+    Storage store=new Storage();
+    
+    
+    
+    @POST
+    public Response addOrders(@PathParam("customerId")String customerId,Order inputOrders){
+        Order searchOrder=store.getOrderList().get(customerId);
+        if(searchOrder==null){
+           throw new CustomerNotFoundException ("Order Relavant to Customer"+customerId+"Not Found");
+        }
+        
+        if(orders.getOrders()==null || orders.getTotal()<=0){
+            throw new InvalidInputException ("Must have data for total and orders");
+        }
+        searchOrder.setTotal(inputOrders.getTotal());
+        searchOrder.setItems(inputOrders.getOrders());
+        
+        store.getOrderList().put(customerId, searchOrder);
+        
+        return Response.status(Response.Status.CREATED)
+                .build();
+    }
     
     
     
