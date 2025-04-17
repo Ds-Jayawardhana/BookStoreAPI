d3edd9b0776b96bd4a9595affa7bc4aa46015a00
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2025-04-17 16:14:28 +0530

    Created the Order Class

--- src/main/java/com/university/bookstore/bookstoreapi/model/Order.java
+++ src/main/java/com/university/bookstore/bookstoreapi/model/Order.java
@@ -4,10 +4,37 @@
  */
 package com.university.bookstore.bookstoreapi.model;
 
+import java.util.Map;
+
 /**
  *
  * @author user
  */
 public class Order {
+
+    public Order() {
+    }
+
+    public Order(int total, Map<Integer, Integer> orders) {
+        this.total = total;
+        this.orders = orders;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
     
+    public Map<Integer, Integer> getOrders() {
+        return orders;
+    }
+
+    public void setItems(Map<Integer, Integer> orders) {
+        this.orders = orders;
+    }
+    private int total;
+    private Map<Integer,Integer>orders;
 }
