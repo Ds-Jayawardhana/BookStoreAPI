b1bac2e3f901cb18e2e448ebe5e0b7ec2e733a38
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2025-04-17 15:59:54 +0530

    Added the main API annoataions

--- src/main/java/com/university/bookstore/bookstoreapi/resources/OrderResource.java
+++ src/main/java/com/university/bookstore/bookstoreapi/resources/OrderResource.java
@@ -4,10 +4,26 @@
  */
 package com.university.bookstore.bookstoreapi.resources;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  *
  * @author user
+ * 
+ OrderResource (/customers/{customerId}/orders)
+o POST /customers/{customerId}/orders
+o GET /customers/{customerId}/orders
+o GET /customers/{customerId}/orders/{orderId}
  */
+
+@Path("/customers/{customerId}/orders")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class OrderResource {
+    
+    
     
 }
